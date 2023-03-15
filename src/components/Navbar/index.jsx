c2676f1443e159472be502966547cb9c2101a808
EliasBenitez <eliasb.benitez@gmail.com>
2023-03-14 22:12:09 -0300

    primer preentrega

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css"
+const Navbar = () => {
+  return (
+     <nav className="container_nav">
+  <a href="">Home</a>
+  <a href="">Piezas</a>
+  <a href="">Combos</a>
+  <img src="" alt="" width="40" height="40"/>
+  
+     </nav> );
+};
+export default Navbar;
